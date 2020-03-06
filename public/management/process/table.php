a91c72c827c68fef3544cf8f72cff40a1194143d
lcodo <laia.codo@bsc.es>
2020-03-06 09:10:51 +0100

    management tag

--- public/management/process/table.php
+++ public/management/process/table.php
@@ -12,25 +12,26 @@ redirectOutside();
                 url: 'https://dev-openebench.bsc.es/vre/applib/processes.php?list',
                 dataSrc: ''
             },
+            autoWidth: false,
             "columns" : [
                 { "data" : "_id" },
                 { "data" : "status" },
-                { "data" : "title" },
-                { "data" : "type" },
-                { "data" : "owner" }
+                { "data" : "inputs_meta.public_ref_dir.data_type" },
+                { "data" : "inputs_meta.public_ref_dir.file_type" },
+                { "data" : "owner"}
             ],
             "columnDefs": [
                 { "title": "_id", "targets": 0 },
                 { "title": "status", "targets": 1 },
-                { "title": "title", "targets": 2 },
-                { "title": "type", "targets": 3 },
+                { "data_type": "title", "targets": 2 },
+                { "fyle_type": "type", "targets": 3 },
                 { "title": "owner", "targets": 4 },
                 { render: function (data, type, row) {
-
+                    var processId = row._id;
                     //status = 0; private
                     //status = 1; public
                     //status = 2; coming soon
-                    var menu = '<select ><option value="" disabled selected> status...</option><option>Private</option><option>Public</option><option>Coming soon</option></select>';
+                    var menu = '<select onChange="obtainStatus(value, name)" name="'+processId+'"><option value="" disabled selected> status...</option><option>Private</option><option>Public</option><option>Coming soon</option></select>';
                     switch(data) {
                         case 0: 
                             return menu + " <span value='0' class='label label-danger'><b>Private</b></span>"; 
@@ -47,5 +48,10 @@ redirectOutside();
                 }, "targets": 1}
             ]
         });
-    });           
+    });
+    
+    function obtainStatus(statusValue, processId) {
+        console.log(statusValue + " " + processId); //Hay que insertar esto
+    }
+    
 </script>
